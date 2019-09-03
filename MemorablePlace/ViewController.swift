329b2d8fd519b61e59213b5b6822941dc7b14e47
jisung <jisung.smart@gmail.com>
2019-09-03 15:44:17 +0900

    Add user location detect methods

--- MemorablePlace/ViewController.swift
+++ MemorablePlace/ViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
+    
+    var manager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        if activePlace != -1 {
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(gestureRecognizer:)))
+        
+        uilpgr.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilpgr)
+        
+        
+        
+        if activePlace == -1 {
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+            
+        }else{
             
             if places.count > activePlace {
                 
@@ -56,6 +74,73 @@ class ViewController: UIViewController, MKMapViewDelegate {
         }
     }
 
+    @objc func longPress(gestureRecognizer: UIGestureRecognizer){
+        
+        if gestureRecognizer.state == UIGestureRecognizer.State.began {
+          
+            let touchPoint = gestureRecognizer.location(in: self.map)
+            
+            let newCoordinate = self.map.convert(touchPoint, toCoordinateFrom: self.map)
+            
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            var title = ""
+            
+            CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+                
+                if error != nil {
+                    print(error!)
+                } else {
+                    
+                    if let placemarks = placemarks?[0]{
+                        
+                        if placemarks.subThoroughfare != nil {
+                            title += placemarks.subThoroughfare! + " "
+                        }
+                        
+                        if placemarks.thoroughfare != nil {
+                            
+                            title += placemarks.thoroughfare! + " "
+                        }
+                        
+                    }
+                } //else end
+                
+                if title == ""{
+                    title = "Added \(NSDate())"
+                }
+                
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = newCoordinate
+                
+                annotation.title = "Test Name"
+                
+                self.map.addAnnotation(annotation)
+                
+                places.append(["name": title , "lat":String(newCoordinate.latitude), "lon":String(newCoordinate.longitude)])
+                
+            
+            }//GLGeocoder end
+           
+        }
+        
+    }//longpress end
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        
+    }
 
+    
+    
 }
 
